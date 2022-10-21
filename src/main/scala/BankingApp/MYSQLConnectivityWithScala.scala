969b77a04c2a7db7a1fdc29ab14ffaaf73d80cc9
Sam Thrower <80697217+samthrower64@users.noreply.github.com>
2022-10-21 13:16:20 +0100

    Create MYSQLConnectivityWithScala.scala

---
+++ src/main/scala/BankingApp/MYSQLConnectivityWithScala.scala
@@ -0,0 +1,9 @@
+package BankingApp
+
+object MYSQLConnectivityWithScala extends App {
+  val driver = "com.mysql.jdbc.Driver"
+  //double tap shift after highlighting whats in the " " and if it shows up in your classes tab, then you're all good
+  val url = "jdbc:mysql://localhost:3306/MySQL80"
+  val username = "root"
+  val password = "password"
+}
